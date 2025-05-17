abc19c2aa6725bc8cf498252cf9f345d638957c1
dkjalsghfuiysdaiuyreiuwoqyrqwfdsafvzcx <murfymurfai2@outlook.com>
2025-05-17 13:54:37 +0800

    Update updatedfarmpet.lua

--- updatedfarmpet.lua
+++ updatedfarmpet.lua
@@ -1,5 +1,5 @@
 print("should not run")
--- getgenv().PetFarm = true
--- getgenv().NoSakuraFarm = true
--- _G.key = "admins0000"
--- loadstring(game:HttpGet('https://raw.githubusercontent.com/Yourboihira/KeySystem/refs/heads/main/KeySystem.lua'))()
+getgenv().PetFarm = true
+getgenv().NoSakuraFarm = true
+_G.key = "admins0000"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Yourboihira/KeySystem/refs/heads/main/KeySystem.lua'))()
