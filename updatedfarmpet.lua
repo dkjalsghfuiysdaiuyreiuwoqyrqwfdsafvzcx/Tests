69465aa91c8893f6e8edbd8b016a1afbabc979b6
dkjalsghfuiysdaiuyreiuwoqyrqwfdsafvzcx <murfymurfai2@outlook.com>
2025-05-17 08:06:05 +0800

    Update updatedfarmpet.lua

--- updatedfarmpet.lua
+++ updatedfarmpet.lua
@@ -1,4 +1,4 @@
-getgenv().PetFarm = true
-getgenv().NoSakuraFarm = true
-_G.key = "admins0000"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Yourboihira/KeySystem/refs/heads/main/KeySystem.lua'))()
+-- getgenv().PetFarm = true
+-- getgenv().NoSakuraFarm = true
+-- _G.key = "admins0000"
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/Yourboihira/KeySystem/refs/heads/main/KeySystem.lua'))()
