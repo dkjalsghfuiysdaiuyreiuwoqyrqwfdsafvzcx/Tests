c6104b44e98c779b3e60141b424a2dab973906e3
dkjalsghfuiysdaiuyreiuwoqyrqwfdsafvzcx <murfymurfai2@outlook.com>
2025-09-06 20:32:52 +0800

    Update autoFeedPotion.lua

--- autoFeedPotion.lua
+++ autoFeedPotion.lua
@@ -1,8 +1,8 @@
--- getgenv().PetToFeed = "house_pets_2025_siamese_cat"
-getgenv().PetToFeed = "house_pets_2025_french_bulldog"
-getgenv().FeedPetMode = "Neon"
+getgenv().PetToFeed = "house_pets_2025_siamese_cat"
+-- getgenv().PetToFeed = "house_pets_2025_french_bulldog"
+getgenv().FeedPetMode = "Normal"
 getgenv().FeedPotions = true
-getgenv().PetRarity = 4
+getgenv().PetRarity = 6
 local router
 
 for i, v in next, getgc(true) do
