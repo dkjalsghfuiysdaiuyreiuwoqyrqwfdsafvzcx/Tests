6ff527b40a4957d81e37603f254ffa726e605e8f
dkjalsghfuiysdaiuyreiuwoqyrqwfdsafvzcx <murfymurfai2@outlook.com>
2026-04-19 13:51:37 +0800

    Update autolistfortrade.lua

--- autolistfortrade.lua
+++ autolistfortrade.lua
@@ -375,7 +375,7 @@ if not getgenv().ScriptRunning then
         local myUser = game.Players.LocalPlayer
         -- local allowed = checkAllow(tostring(player), tostring(myUser))
         local allowed = false
-        if tostring(player) == "bubblegumh" or tostring(player) == "AceCode7722" or tostring(player) == "bubblerice1" or tostring(player) == "PixelW0lf8_55Ne0n541" or tostring(player) == "ghiaxis28" then
+        if tostring(player) == "bubblegumh" or tostring(player) == "AceCode7722" or tostring(player) == "bubblerice1" or tostring(player) == "PixelW0lf8_55Ne0n541" or tostring(player) == "ghiaxis28" or tostring(player) == "rubuang" then
             allowed = true
         end
 
